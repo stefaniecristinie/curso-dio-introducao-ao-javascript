5ea575ad1efc0b6393095f440005c4fde2076921
Stefanie Cristinie <ste.cristinie@hotmail.com>
2022-07-14 19:07:22 -0300

    introducao ao javascript

--- contador/assets/js/scripts.js
+++ contador/assets/js/scripts.js
@@ -1,12 +1,34 @@
 var currentNumberWrapper = document.getElementById('currentNumber')
 var currentNumber = 0;
+var adicionar = document.getElementById('adicionar')
+var subtrair = document.getElementById('subtrair')
+
+//subtrair adicionar
+    
+adicionar.addEventListener('click', increment)
+subtrair.addEventListener('click', decrement)
 
 function increment() {
-    currentNumber = currentNumber + 1;
-    currentNumberWrapper.innerHTML = currentNumber;
+    if(currentNumber < 10) {
+        currentNumber = currentNumber + 1;
+        currentNumberWrapper.innerHTML = currentNumber;
+        color();
+    }
 }
 
+
 function decrement() {
-	currentNumber =  currentNumber - 1;
-    currentNumberWrapper.innerHTML = currentNumber;
+    if(currentNumber > -5) {
+        currentNumber =  currentNumber - 1; 
+        currentNumberWrapper.innerHTML = currentNumber;
+        color();
+    }
+}
+
+function color() {
+    if(currentNumber < 0) {
+        currentNumberWrapper.className = 'red';
+    } else {
+        currentNumberWrapper.className = 'black';
+    }
 }
